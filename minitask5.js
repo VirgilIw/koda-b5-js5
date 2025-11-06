5833007cac9a09da6ddb306c372fa21e6635b48f
VirgilIw <imanuelambar11@gmail.com>
2025-11-06 13:35:42 +0700

    update comment
    line 2 ada pesan comment

--- minitask5.js
+++ minitask5.js
@@ -1,9 +1,9 @@
 const account = { id: 1, userName: "virgil", password: "12345" };
-
-const newPassword = "45678"; // password baru yg mau diubah
-
+// password sebelumnya sudah tervalidasi dan lebih dari 5
+const newPassword = "4568"; // password baru yg mau diubah
+let batas = 5;
 if (newPassword.length < 5) {
-  console.log("minimum password 5");
+  console.log(`minimum password = ${batas}`);
 } else {
   const changePassword = { ...account, password: newPassword };
   console.log(changePassword);
